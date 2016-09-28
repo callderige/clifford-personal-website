4b54f966e239027dcc716595c5d548cd948a0b62
Cliff Allderige <ca.allderige@gmail.com>
2016-09-28 00:12:39 -0400

    navbar active link now works.

--- js/fadein.js
+++ js/fadein.js
@@ -1,16 +1,22 @@
 $('.about-link').click(function() {
+	$(this).parent().siblings('li').removeClass('active');
+	$(this).parent().addClass('active');
  	$(".resume-container").fadeOut(250);
   	$(".code-container").fadeOut(250);
   	$(".about-container").delay(250).fadeIn(250);
 });
 
 $('.resume-link').click(function() {
+	$(this).parent().siblings('li').removeClass('active');
+	$(this).parent().addClass('active');
 	$(".about-container").fadeOut(250);
 	$(".code-container").fadeOut(250);
   	$(".resume-container").delay(250).fadeIn(250);
 });
 
 $('.code-link').click(function() {
+	$(this).parent().siblings('li').removeClass('active');
+	$(this).parent().addClass('active');
   	$(".about-container").fadeOut(250);
   	$(".resume-container").fadeOut(250);
   	$(".code-container").delay(250).fadeIn(250);
